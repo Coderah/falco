93b9aea53a0e9bc9eee53f4fb653b3d3d3fbf356
Alex Howard <alex@howardsupply.com>
2014-04-04 23:15:57 -0700

    research: added 'Object.create' polyfill

---
+++ research/polyfills/object-create.js
@@ -0,0 +1,16 @@
+// http://jsperf.com/new-vs-object-create-including-polyfill
+if (typeof Object.create !== 'function') {
+   Object.create = function(o, props) {
+      function F() {}
+      F.prototype = o;
+
+      if (typeof(props) === "object") {
+             for (var prop in props) {
+                if (props.hasOwnProperty((prop))) {
+                   F[prop] = props[prop];
+               }
+           }
+       }
+       return new F();
+    };
+}
