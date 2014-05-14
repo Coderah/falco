3ce6a1b3c245cc729e9b9e7ef7772fd14f93e3b5
Alex Howard <alex@howardsupply.com>
2014-05-14 09:19:28 -0700

    research: move deserialize to whiskers object, add else for each in example template, fixes

--- research/template.js
+++ research/template.js
@@ -1,81 +1,6 @@
-var pageTemplate = (function () {
-    var fragment, schema;
+var whiskers = {
 
-    deserializePatternSchema = {
-        page: {
-            createdDate: { // section > article > small.meta
-                type: 'textContent',
-                element: function(targetEl) {
-                    var el = targetEl.querySelector('article>small');
-                    if (el) return el.childNodes[1];
-
-                    return false;
-                }
-            },
-            title: { // section > article > h1
-                type: 'textContent',
-                element: function(targetEl) {
-                    return targetEl.querySelector('article>h1');
-                }
-            },
-            type: {
-                type: 'class',
-                element: function(targetEl) {
-                    return targetEl.childNodes[0];
-                }
-            }
-        }
-    };
-
-    function create(parameters, options) {
-        if (!fragment) {
-            fragment = document.createDocumentFragment();
-
-            var section = document.createElement('section');
-            section.classList.add('main-section');
-
-            var article = document.createElement('article');
-            section.appendChild(article);
-
-            var h1 = document.createElement('h1');
-            article.appendChild(h1);
-
-            var smallMeta = document.createElement('small'),
-                smallMetaPublishDateText = document.createTextNode('');
-            smallMeta.textContent = 'published on ';
-            smallMeta.appendChild(smallMetaPublishDateText);
-            article.appendChild(smallMeta);
-
-            var p = document.createElement('p'),
-                pData = [
-                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                    'Ipsa, est, ex, nesciunt, aliquam quia sed sequi officiis necessitatibus eum?',
-                    'Fugit, nostrum, consequatur consequuntur saepe sint qui iusto corporis eaque iste quibusdam.',
-                    'Provident, nulla, repellat nesciunt similique quibusdam dolor architecto delectus earum unde culpa ea minima!'
-                ];
-
-            for (var i = 0; i < 4; i++) {
-                var pi = (i < 4 ? p.cloneNode(false) : p);
-
-                pi.textContent = pData[i];
-                article.appendChild(pi);
-            }
-
-            fragment.appendChild(section);
-        }
-
-        var nodes = fragment.cloneNode(true);
-
-        bind(nodes, deserializePatternSchema);
-
-        if (parameters) {
-            nodes.childNodes[0].update(parameters);
-        }
-
-        return nodes.childNodes[0];
-    }
-
-    function bind(nodes, schema) {
+    bind: function(nodes, schema) {
         var node = nodes.childNodes[0];
         var deserializePatterns = {};
 
@@ -96,10 +21,10 @@ var pageTemplate = (function () {
 
         createPattern(schema, deserializePatterns);
 
-        node.update = deserialize.bind(nodes, deserializePatterns);
-    }
+        node.update = whiskers.deserialize.bind(nodes, deserializePatterns);
+    },
 
-    function deserialize(patterns, parameters) {
+    deserialize: function(patterns, parameters) {
         for (var i in parameters) {
             var pattern = patterns[i];
             if (pattern && pattern.hasOwnProperty('element') && pattern.element.nodeType) {
@@ -128,10 +53,105 @@ var pageTemplate = (function () {
                         break;
                 }
             } else if (patterns.hasOwnProperty(i)) {
-                deserialize(patterns[i], parameters[i]);
+                whiskers.deserialize(patterns[i], parameters[i]);
+            }
+        }
+    }
+};
+
+var pageTemplate = (function () {
+    var fragment, elseFragment, schema;
+
+    var eachCommentsPartial = (function () {
+        var fragment, schema;
+        
+        var deserializePatternSchema = {
+
+        };
+
+        return function (parameters, options) {
+            if (!fragment) {
+                
+            }
+
+            if (!elseFragment) {
+
             }
         };
-    }
+    })();
 
-    return create;
+    var deserializePatternSchema = {
+        page: {
+            createdDate: { // section > article > small.meta
+                type: 'textContent',
+                element: function(targetEl) {
+                    var el = targetEl.querySelector('article>small');
+                    if (el) return el.childNodes[1];
+
+                    return false;
+                }
+            },
+            title: { // section > article > h1
+                type: 'textContent',
+                element: function(targetEl) {
+                    return targetEl.querySelector('article>h1');
+                }
+            },
+            type: {
+                type: 'class',
+                element: function(targetEl) {
+                    return targetEl.childNodes[0];
+                }
+            }
+        }
+    };
+
+    return function (parameters, options) {
+        if (!fragment) {
+            fragment = document.createDocumentFragment();
+
+            var section = document.createElement('section');
+            section.classList.add('main-section');
+
+            var article = document.createElement('article');
+            section.appendChild(article);
+
+            var h1 = document.createElement('h1');
+            article.appendChild(h1);
+
+            var smallMeta = document.createElement('small'),
+                smallMetaPublishDateText = document.createTextNode('');
+            smallMeta.classList.add('meta');
+            smallMeta.textContent = 'published on ';
+            smallMeta.appendChild(smallMetaPublishDateText);
+            article.appendChild(smallMeta);
+
+            var p = document.createElement('p'),
+                pData = [
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                    'Ipsa, est, ex, nesciunt, aliquam quia sed sequi officiis necessitatibus eum?',
+                    'Fugit, nostrum, consequatur consequuntur saepe sint qui iusto corporis eaque iste quibusdam.',
+                    'Provident, nulla, repellat nesciunt similique quibusdam dolor architecto delectus earum unde culpa ea minima!'
+                ];
+
+            for (var i = 0; i < 4; i++) {
+                var pi = (i < 4 ? p.cloneNode(false) : p);
+
+                pi.textContent = pData[i];
+                article.appendChild(pi);
+            }
+
+            fragment.appendChild(section);
+        }
+
+        var nodes = fragment.cloneNode(true);
+
+        whiskers.bind(nodes, deserializePatternSchema);
+
+        if (parameters) {
+            nodes.childNodes[0].update(parameters);
+        }
+
+        return nodes.childNodes[0];
+    };
 })();
