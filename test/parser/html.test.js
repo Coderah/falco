2f0ae0fc398545eef309b88e2fcdf0b02e559620
Alex Howard <alex@howardsupply.com>
2014-09-10 12:17:58 -0700

    cleanup; fix tests for async parser

--- test/parser/html.test.js
+++ test/parser/html.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect.js');
-var lex = require('../../lib/parser.js');
+var Parser = require('../../lib/parser.js');
 
 function expectToken(token, type, name) {
     expect(token).to.be.a(Object);
@@ -19,74 +19,87 @@ function expectCloseTag(token, tagName) { expectToken(token, 'closeTag', tagName
 
 describe('html tokenization', function() {
     describe('element', function() {
-        it('self-closing', function() {
-            var tokens = lex('<br />');
+        it('self-closing', function(done) {
+            Parser('<br />', function(tokens) {
+                var openToken = tokens[0];
+                var closeToken = tokens[1];
 
-            var openToken = tokens[0];
-            var closeToken = tokens[1];
+                expectOpenTag(openToken, 'br');
+                expectCloseTag(closeToken, 'br');
 
-            expectOpenTag(openToken, 'br');
-            expectCloseTag(closeToken, 'br');
+                done();
+            });
         });
 
-        it('opening and closing', function() {
-            var tokens = lex('<p></p>');
-            expect(tokens).to.have.length(3);
+        it('opening and closing', function(done) {
+            Parser('<p></p>', function(tokens) {
+                expect(tokens).to.have.length(3);
 
-            var openToken = tokens[0];
-            var inElementToken = tokens[1];
-            var closeToken = tokens[2];
+                var openToken = tokens[0];
+                var inElementToken = tokens[1];
+                var closeToken = tokens[2];
 
-            expectOpenTag(openToken, 'p');
-            expectToken(inElementToken, 'inElement');
-            expectCloseTag(closeToken, 'p');
+                expectOpenTag(openToken, 'p');
+                expectToken(inElementToken, 'inElement');
+                expectCloseTag(closeToken, 'p');
+
+                done();
+            });
         });
 
-        it('content', function() {
+        it('content', function(done) {
             var text = 'lorem ipsum dolor sit amet';
-            var tokens = lex('<p>' + text + '</p>');
+            Parser('<p>' + text + '</p>', function(tokens) {
+                var textToken = tokens[2];
 
-            var textToken = tokens[2];
+                expectToken(textToken, 'text');
+                expect(textToken.value).to.equal(text);
 
-            expectToken(textToken, 'text');
-            expect(textToken.value).to.equal(text);
+                done();
+            });
         });
 
         describe('nested', function() {
-            it('one-level', function() {
+            it('one-level', function(done) {
                 var nestedTagName = 'article';
-                var tokens = lex('<section><' + nestedTagName + '></' + nestedTagName + '></section>');
+                Parser('<section><' + nestedTagName + '></' + nestedTagName + '></section>', function(tokens) {
+                    var inFirstToken = tokens[1];
+                    var openToken = tokens[2];
+                    var closeToken = tokens[4];
 
-                var inFirstToken = tokens[1];
-                var openToken = tokens[2];
-                var closeToken = tokens[4];
+                    expectToken(inFirstToken, 'inElement');
 
-                expectToken(inFirstToken, 'inElement');
+                    expectOpenTag(openToken, nestedTagName);
+                    expectCloseTag(closeToken, nestedTagName);
 
-                expectOpenTag(openToken, nestedTagName);
-                expectCloseTag(closeToken, nestedTagName);
+                    done();
+                });
             });
         });
 
         describe('attribute', function() {
-            it('empty', function() {
-                var tokens = lex('<input readonly />');
+            it('empty', function(done) {
+                Parser('<input readonly />', function(tokens) {
+                    var attributeToken = tokens[1];
 
-                var attributeToken = tokens[1];
+                    expect(attributeToken, 'tagAttribute');
+                    expect(attributeToken.name).to.equal('readonly');
+                    expect(attributeToken).to.not.have.property('value');
 
-                expect(attributeToken, 'tagAttribute');
-                expect(attributeToken.name).to.equal('readonly');
-                expect(attributeToken).to.not.have.property('value');
+                    done();
+                });
             });
 
-            it('valued', function() {
-                var tokens = lex('<a href="/home">whiskers</a>');
+            it('valued', function(done) {
+                Parser('<a href="/home">whiskers</a>', function(tokens) {
+                    var attributeToken = tokens[1];
 
-                var attributeToken = tokens[1];
+                    expect(attributeToken, 'tagAttribute');
+                    expect(attributeToken.name).to.equal('href');
+                    expect(attributeToken.value).to.equal('/home');
 
-                expect(attributeToken, 'tagAttribute');
-                expect(attributeToken.name).to.equal('href');
-                expect(attributeToken.value).to.equal('/home');
+                    done();
+                });
             });
         });
     });
