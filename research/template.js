9db6d7617e5cae9096ac2aa62887ef5e5a2946c7
Coderah <alex@howardsupply.com>
2015-04-07 20:05:49 -0700

    more simplification; fixes; each partial and implementation; more tests and benchmarking

--- research/template.js
+++ research/template.js
@@ -1,30 +1,213 @@
 var whiskers = (function () {
-    var uid = -1, cache = [];
+    var cache = [];
 
     var whiskers = {};
 
     whiskers.cache = function(node) {
         if (!node.nodeType) return Error('invalid node');
 
-        var elCacheId = node.attributes['data-cache-id'];
+        var cacheId = node.getAttribute('data-cache-id');
 
-        if (!elCacheId) {
-            uid++;
-            cache[uid] = {};
+        if (!cacheId) {
+            cache.push({});
+            cacheId = cache.length - 1;
 
-            node.attributes['data-cache-id'] = uid;
+            node.setAttribute('data-cache-id', cacheId);
         }
 
-        return cache[elCacheId || uid];
+        return cache[cacheId];
     };
+
+    whiskers.updateClass = function(element, value, cacheName) {
+        var cache = whiskers.cache(element);
+        var oldValue = cache[cacheName];
+        var classList = element.classList;
+
+        if (classList.contains(oldValue)) {
+            classList.remove(oldValue);
+        }
+
+        if (!classList.contains(value)) {
+            classList.add(value);
+        }
+
+        cache[cacheName] = value;
+    }
+
+    whiskers.insertAfter = function(newNode, referenceNode) {
+        var nextSibling = referenceNode.nextSibling;
+        if (nextSibling) {
+            referenceNode.parentNode.insertBefore(newNode, nextSibling);
+        } else {
+            referenceNode.parentNode.appendChild(newNode);
+        }
+    }
+
+    whiskers.nodeListToArray = function(nl) {
+        var arr = [];
+        for(var i = nl.length; i--; arr.unshift(nl[i]));
+        return arr;
+    }
 
     return whiskers;
 })(); 
 
-var pageTemplate = (function () {
+var PageTemplate = (function () {
     var fragment;
 
-    var template = function (parameters, options) {
+    var CommentsPartial = (function () {
+        var fragment, elseFragment;
+
+        function Template(parameters, option) {
+            if (!fragment) {
+                fragment = document.createDocumentFragment();
+
+                var article = document.createElement('article');
+
+                var h2IndexTitle = document.createElement('h2');
+                h2IndexTitle.appendChild(document.createTextNode('')); // @index
+                h2IndexTitle.appendChild(document.createTextNode(' - '));
+                h2IndexTitle.appendChild(document.createTextNode('')); // title
+                article.appendChild(h2IndexTitle);
+
+                var smallMeta = document.createElement('small');
+                smallMeta.classList.add('meta');
+                smallMeta.appendChild(document.createTextNode('')); // username
+                smallMeta.appendChild(document.createTextNode(' posted on '));
+                smallMeta.appendChild(document.createTextNode('')); // createdDate
+                article.appendChild(smallMeta);
+
+                var paragraph = document.createElement('p');
+                paragraph.appendChild(document.createTextNode('')); //content
+                article.appendChild(paragraph);
+
+                fragment.appendChild(article);
+            }
+
+            if (!elseFragment) {
+                elseFragment = document.createDocumentFragment();
+
+                var article = document.createElement('article');
+                article.classList.add('message');
+
+                var paragraph = document.createElement('p');
+                paragraph.textContent = 'Be the first to comment.';
+
+                elseFragment.appendChild(paragraph);
+            }
+
+            if (parameters) {
+                this.build(parameters.hasOwnProperty('comments') && parameters.page.comments.length);
+                this.update(parameters);
+            } else {
+                this.build();
+            }
+
+            return this;
+        }
+
+        Template.prototype.appendTo = function(node) {
+            var nodes = this.nodes || this.elseNodes;
+
+            if (nodes.length === 1) {
+                node.appendChild(nodes[0]);
+            } else {
+                for (var i = 0; i < nodes.length; i++) {
+                    node.appendChild(nodes[i]);
+                }
+            }
+        };
+
+        Template.prototype.build = function(condition) {
+            // NOTE: this only builds each fragment once, so if we switch back and forth we don't memory churn
+
+            if (condition && !this.nodes) {
+                this.nodes = [document.importNode(fragment, true).childNodes[0]];
+
+                this.bind();
+            } else if (!this.elseNodes) {
+                this.elseNodes = [document.importNode(elseFragment, true).childNodes[0]];
+
+                // NOTE: no bind here because the else fragment is static
+            }
+        };
+
+        Template.prototype.bind = function (nodeIndex) {
+            // NOTE: bind is simplified because elseFragment is fully static
+
+            if (typeof nodeIndex === 'number') {
+                if (this.bound.comments.length -1 >= nodeIndex) return this.bound.comments[nodeIndex];
+
+                return this.bound.comments[nodeIndex] = {
+                    '@index': this.nodes[nodeIndex].childNodes[0].childNodes[0],
+                    title: this.nodes[nodeIndex].childNodes[0].childNodes[2],
+                    username: this.nodes[nodeIndex].childNodes[1].childNodes[0],
+                    createdDate: this.nodes[nodeIndex].childNodes[1].childNodes[2],
+                    content: this.nodes[nodeIndex].childNodes[2].childNodes[0]
+                };
+            } else if (this.nodes) {
+                this.bound = {
+                    comments: []
+                };
+
+                return this.bind(0);
+            }
+        };
+
+        Template.prototype.update = function(parameters, options) {
+            this.parameters = parameters;
+            if (parameters.page.comments) {
+                if (parameters.page.comments.length) {
+                    this.build(true);
+
+                    for (var i = 0; i < parameters.page.comments.length; i++) {
+                        var node, comment = parameters.page.comments[i];
+
+                        if (this.nodes.length -1 >= i) {
+                            node = this.nodes[i];
+                        } else {
+                            node = document.importNode(fragment, true).childNodes[0];
+                            this.nodes[i] = node;
+                        }
+                        
+                        var bound = this.bind(i);
+
+                        bound['@index'].textContent = i;
+                        if (comment.title) bound.title.textContent = comment.title;
+                        if (comment.username) bound.username.textContent = comment.username;
+                        if (comment.createdDate) bound.createdDate.textContent = comment.createdDate;
+                        if (comment.content) bound.content.textContent = comment.content;
+                    };
+
+                    if (this.elseNodes && this.elseNodes[0].parentElement) {
+                        this.elseNodes[0].parentElement.replaceChild(this.nodes[0], this.elseNodes[0]);
+                    }
+
+                    if (parameters.page.comments.length > 1 && this.nodes[0].parentElement) {
+                        for (var i = 1; i < parameters.page.comments.length; i++) {
+                            var node = this.nodes[i];
+
+                            if (!node.parentElement || node.parentElement !== this.nodes[0].parentElement) {
+                                whiskers.insertAfter(node, this.nodes[0]);
+                            }
+                        }
+                    }
+                } else {
+                    this.build(false);
+
+                    if (this.nodes && this.nodes[0].parentElement) {
+                        this.nodes[0].parentElement.replaceChild(this.elseNodes[0], this.nodes[0]);
+                        // NOTE: if there are multiple top-level elements for the fragment we would add code
+                        //       to whiskers.insertAfter those elements
+                    }
+                }
+            }
+        };
+
+        return Template;
+    })(); 
+
+    function Template(parameters, options) {
         if (!fragment) {
             fragment = document.createDocumentFragment();
 
@@ -60,24 +243,50 @@ var pageTemplate = (function () {
             }
 
             fragment.appendChild(section);
+
+            var commentsSection = document.createElement('section');
+            commentsSection.classList.add('comments');
+
+            fragment.appendChild(commentsSection);
         }
 
-        var nodes = this.nodes = fragment.cloneNode(true);
 
-        this.bound = {
-            createdDate: nodes.childNodes[0].querySelector('article>small').childNodes[1],
-            title: nodes.childNodes[0].querySelector('article>h1'),
-            type: nodes.childNodes[0].childNodes[0]
-        };
+        var nodes = this.nodes = whiskers.nodeListToArray(document.importNode(fragment, true).childNodes);
+
+        this.bind();
+
+        this.eachComments = new CommentsPartial(parameters);
+        this.eachComments.appendTo(this.nodes[1]);
 
         if (parameters) {
-            this.update(parameters);
+            this.update(parameters, options);
         }
 
         return this;
     };
 
-    template.prototype.update = function(parameters) {
+    Template.prototype.appendTo = function(node) {
+        var nodes = this.nodes;
+
+        if (nodes.length === 1) {
+            node.appendChild(nodes[0]);
+        } else {
+            for (var i = 0; i < nodes.length; i++) {
+                node.appendChild(nodes[i]);
+            }
+        }
+    };
+
+    Template.prototype.bind = function() {
+        this.bound = {
+            createdDate: this.nodes[0].querySelector('article>small').childNodes[1],
+            title: this.nodes[0].querySelector('article>h1'),
+            type: this.nodes[0].childNodes[0]
+        };
+    }
+
+    Template.prototype.update = function(parameters, options) {
+        this.parameters = parameters;
         if (!parameters) return;
 
         if (parameters.page) {
@@ -90,22 +299,12 @@ var pageTemplate = (function () {
             }
 
             if (parameters.page.type) {
-                var cache = whiskers.cache(this.bound.type);
-                var oldValue = cache.typeValue;
-                var classList = this.bound.type.classList;
-
-                if (classList.contains(oldValue)) {
-                    classList.remove(oldValue);
-                }
-
-                if (!classList.contains(parameters.page.type)) {
-                    classList.add(parameters.page.type);
-                }
-
-                cache.typeValue = parameters.page.type;
+                whiskers.updateClass(this.bound.type, parameters.page.type, 'pageType');
             }
+
+            if (parameters.page.comments) this.eachComments.update(parameters, options);
         }
     }
 
-    return template;
+    return Template;
 })();
